764ea5b8017fd463725a12800485feb3f85a0981
Jessica Joly & Lisa Folliard <student@epicodus.com>
2015-09-10 10:57:33 -0700

    Add HTML and jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,19 @@ var countUp = function(total, multiple) {
 
    for(var i = multiple; i <= total; i += multiple){
      output.push(i);
-
    }
    return output;
 };
+
+$(document).ready(function() {
+  $("form#count_up").submit(function(event) {
+    var total = parseInt($("input#total").val());
+    var multiple = parseInt($("input#multiple").val());
+    var output = countUp(total, multiple);
+
+    $(".output").text(output);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
